9541dda7849e65b34833c8f7ddf84e331560d31b
manishkumarmandane <bm0369@gmail.com>
2022-07-27 21:18:52 +0100

    modi

---
+++ IFeleseif.java
@@ -0,0 +1,30 @@
+package Ifelseif;
+
+public class IFeleseif
+{
+    public static void main(String[] args)
+    {
+    //   double  i=0.0;
+        double a = -10;
+        int i = (int)a;
+
+        if(i>0)
+        {
+            System.out.println("postive");
+        } else if(i<0)
+        {
+            System.out.println("negative");
+            System.out.println("Manish");
+        }
+    //   else if(!(i>=0)&&(i<=-0))
+      // {
+        //    System.out.println("zero");
+          //System.out.println("negative zero is not alloater");
+        //}
+        else
+        {
+          // System.out.println("negative zero is not alloater");
+            System.out.println("zero");
+        }
+    }
+}
